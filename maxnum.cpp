7a59076df8ed7d3cfb4629fef539c3f174bf1cec
dishita-222 <ds2890@srmist.edu.in>
2023-03-19 17:55:24 +0530

    commit

---
+++ maxnum.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using  namespace std;
+
+int main(){
+    int a,b,c;
+    cin>>a>>b>>c;
+    if(a>b && a>c)
+        
+            cout<<"largest number is "<<a<<endl;
+        
+       
+        
+        else if (b>a && b>c)
+            cout<<"largest number is "<<c<<endl;
+
+        
+
+        else
+            cout<<"Largest number is "<<c<<endl;
+        
+    
+ 
+      return 0;
+
+} 
