1ed60025dcb4437d72af10832a3041a282c9552e
dishita-222 <ds2890@srmist.edu.in>
2023-03-22 16:35:38 +0530

    update

---
+++ oddeven.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using  namespace std;
+
+int main(){
+    int n ;
+    cout<<"enter the number: ";
+    cin>>n;
+    if(n%2==0)
+    cout<<"Number is even ";
+
+    else 
+    cout<<"Odd ";
+
+      return 0;
+
+} 
